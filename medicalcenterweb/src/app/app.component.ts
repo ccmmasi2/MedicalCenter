a04f0d0eb8c586d66cb8dc5f0d7e99b31a61c453
cmontenegro <cmontenegro>
2024-08-15 12:49:20 -0500

    fix bugs

--- medicalcenterweb/src/app/app.component.ts
+++ medicalcenterweb/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AlertService } from './services/alert-service.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html'
 })
 
 export class AppComponent {
